8aca3154f6fb37a387433bbfca6c605e16cea14e
Rafael Ernesto Ferro <rafix81@gmail.com>
2016-03-03 22:10:24 +0000

    [wip] migración a sylius 0.17.*@dev

--- web/app.php
+++ web/app.php
@@ -2,17 +2,16 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\Debug\Debug;
 
-//$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 //$loader = new ApcClassLoader('sylius', $loader);
 //$loader->register(true);
-require_once __DIR__.'/../app/bootstrap.php.cache';
+
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
-//Debug::enable();
-$kernel = new AppKernel('prod', true);
+
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,34 +1,22 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
 
-// if (false &&( isset($_SERVER['HTTP_CLIENT_IP'])
-//     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-//     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('201.213.156.204','127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
-// )) {
-//     header('HTTP/1.0 403 Forbidden');
-//     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-// }
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('192.168.10.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
 
-// require_once __DIR__.'/../app/bootstrap.php.cache';
-// require_once __DIR__.'/../app/AppKernel.php';
-
-// $kernel = new AppKernel('dev', true);
-// $kernel->loadClassCache();
-
-// $request = Request::createFromGlobals();
-
-
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-$loader = require_once __DIR__.'/../app/autoload.php';
-Debug::enable();
+require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
-// $kernel->loadClassCache();
-$request = Request::createFromGlobals();
+$kernel->loadClassCache();
 
+$request = Request::createFromGlobals();
 Request::enableHttpMethodParameterOverride();
 
 $response = $kernel->handle($request);
